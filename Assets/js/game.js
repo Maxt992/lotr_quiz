a74895f96f71979b658b6ffdaff87b99357762dd
Maxwell Turner <maxt992@gmail.com>
2022-06-13 18:31:54 -0700

    Pseudocodeing JS

---
+++ Assets/js/game.js
@@ -0,0 +1,50 @@
+// DECLARE 'timer' number
+
+// DECLARE 'questions' array
+var questions = [
+    {
+        // questions
+        // answers
+        // correct answer
+    },
+    {
+        // questions
+        // answers
+        // correct answer
+    },
+    {
+        // questions
+        // answers
+        // correct answer
+    },
+    // and so on...
+];
+
+// DECLARE the 'timeEl'
+var timeEl = document.getElementById('timer');
+// OR document.querySelector('#timer'); would work || querySelectorAll grabs ALL elements with id tag.
+
+// Function 'startGame'
+
+    // Hide the start screen
+
+    // Display the first question
+
+    // Set the starting value of 'countdown'
+
+    // Start the timer 'startTimer'
+
+// Function 'answerQuestion'
+
+    // Check if the selected answer is correct
+
+        // IF the answer is wrong
+
+            // THEN we need to subtract from 'countdown'
+
+            // Optional: DISPLAY "right" or "wrong"
+
+        // Display the next question
+
+// Function 'displayCurrentQuestion'
+
