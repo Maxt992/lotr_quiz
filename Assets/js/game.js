c1806030614162d55aba8af8ec58db4f47d41a58
Maxwell Turner <maxt992@gmail.com>
2022-06-27 14:20:52 -0700

    edit

--- Assets/js/game.js
+++ Assets/js/game.js
@@ -109,6 +109,7 @@ function showQuestions(index) {
 let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
 let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>'; 
 
+
 function optionSelected(answer) {
     clearInterval(counter);
     clearInterval(counterLine);
